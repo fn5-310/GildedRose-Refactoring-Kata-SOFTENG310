3818f0ad97e444c98bf7e79eda1781ec8acbe7e7
MonkePeek <gtal900@aubcklanduni.ac.nz>
2026-08-10 15:05:27 +1200

    Converted magic numbers into variables (George)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,11 @@ class GildedRose {
     private static final String BRIE = "Aged Brie";
     private static final String PASSES = "Backstage passes to a TAFKAL80ETC concert";
     private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private static final int zeroQuality = 0;
+    private static final int fiftyQuality = 50;
+    private static final int sellInZero = 0;
+    private static final int sellInHigh = 11;
+    private static final int sellInLow = 6;
     Item[] items;
 
     public GildedRose(Item[] items) {
@@ -14,20 +19,20 @@ class GildedRose {
         for (Item item : items) {
             if (!item.name.equals(BRIE)
                     && !item.name.equals(PASSES)) {
-                if (item.quality > 0) {
+                if (item.quality > zeroQuality) {
                     if (!item.name.equals(SULFURAS)) {
                         item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (item.quality < 50) {
+                if (item.quality < fiftyQuality) {
                     item.quality = item.quality + 1;
                     if (item.name.equals(PASSES)) {
                         // Increase quality by 1 if sellIn is less than 11 and quality is below 50
                         // If sellIn is below 6 and quality is still below 50, increase quality by 1 again
-                        if (item.sellIn < 11 && item.quality < 50) {
+                        if (item.sellIn < sellInHigh && item.quality < fiftyQuality) {
                             item.quality = item.quality + 1;
-                            if (item.sellIn < 6 && item.quality < 50) {
+                            if (item.sellIn < sellInLow && item.quality < fiftyQuality) {
                                 item.quality = item.quality + 1;
                             }
                         }
@@ -39,10 +44,10 @@ class GildedRose {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sellIn < 0) {
+            if (item.sellIn < sellInZero) {
                 if (!item.name.equals(BRIE)) {
                     if (!item.name.equals(PASSES)) {
-                        if (item.quality > 0) {
+                        if (item.quality > zeroQuality) {
                             if (!item.name.equals(SULFURAS)) {
                                 item.quality = item.quality - 1;
                             }
@@ -51,7 +56,7 @@ class GildedRose {
                         item.quality = item.quality - item.quality;
                     }
                 } else {
-                    if (item.quality < 50) {
+                    if (item.quality < fiftyQuality) {
                         item.quality = item.quality + 1;
                     }
                 }
